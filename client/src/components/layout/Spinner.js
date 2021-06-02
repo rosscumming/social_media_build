a84088b2af83e0519c0ec68f28c2c59faa254ed3
Ross Cumming <rosso5792@gmail.com>
2021-06-02 09:08:51 +0100

    working on dashboard & profile management setup

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <>
+      <i
+        class='fa fa-spinner fa-spin fa-6x'
+        style={{
+          margin: 'auto',
+          display: 'block',
+          textAlign: 'center',
+          fontSize: '30px',
+        }}
+      ></i>{' '}
+    </>
+  );
+};
+
+export default Spinner;
